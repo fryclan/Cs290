c885840e1d2cbd20f509b21a9da5e20800abab7a
fryclan <fry.jeffery123454@gmail.com>
2024-03-15 11:42:45 -0700

    why no collapsing

---
+++ CS290/FinalWithJason/scripts/Leaderboard.js
@@ -0,0 +1,24 @@
+function Leaderboard()
+{
+    var x = document.getElementById("LeaderboardDiv");
+    if (x.style.display === "block")
+    {
+      x.style.display = "none";
+    }
+    else
+    {
+      x.style.display = "block";
+    }
+}
+
+
+loadDoc()
+{
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = funtion()
+    {
+        document.getElementById("Leaderboard").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "Leaderboard.txt");
+    xhttp.send();
+}
